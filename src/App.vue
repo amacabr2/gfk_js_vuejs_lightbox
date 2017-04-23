6d2e1940d199c93af8ea116d5b77df0c13ff9d2c
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-04-23 10:40:11 +0200

    Lightbox.vue

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,24 @@
         <a href="http://lorempicsum.com/futurama/800/600/1" v-lightbox>
             <img src="http://lorempicsum.com/futurama/150/150/1" alt="">
         </a>
+        <lightbox></lightbox>
     </div>
 </template>
 
 <script>
+
+    import Ligthbox from './components/lightbox/Lightbox.vue'
+
     export default {
-        name: 'app'
+
+        name: 'app',
+
+        components: {
+            Ligthbox
+        }
+
     }
+
 </script>
 
 <style>
---
+++ src/components/lightbox/Lightbox.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{ url }}
+    </div>
+</template>
+
+<script>
+
+    export default {
+
+        data() {
+            return {
+                url: '???'
+            }
+        }
+
+    }
+
+</script>
