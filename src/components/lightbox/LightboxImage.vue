07b84f8710608eda88b1c69d313f368061348fbc
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-04-23 11:47:35 +0200

    Load of the good image

--- src/components/lightbox/LightboxImage.vue
+++ src/components/lightbox/LightboxImage.vue
@@ -1,12 +1,61 @@
 <template>
 
-    <div></div>
+    <div v-if="loading">Chargement...</div>
+
+    <img class="lightbox_image" :src="src" :style="style">
 
 </template>
 
 <script>
 
     export default {
+
+        props: {
+            image: String
+        },
+
+        data() {
+            return {
+                loading: true,
+                src: false,
+                style: {}
+            }
+        },
+
+        mounted() {
+
+            let image = new window.Image();
+
+            image.onload = _ => {
+
+                this.loading = false;
+                this.src = this.image;
+                let width = image.width;
+                let height = image.height;
+
+                if (width > window.innerWidth || height > window.innerHeight) {
+                    let ratio = width / height;
+                    let windowRatio = window.innerWidth / window.innerHeight;
+                    if (ratio > windowRatio) {
+                        width = window.innerWidth;
+                        height = width / ratio;
+                    } else {
+                        height = window.innerHeight;
+                        width = height * ratio;
+                    }
+                }
+
+                this.style = {
+                    width: width + 'px',
+                    height: height + 'px',
+                    top: ((window.innerHeight - height) * 0.5) + 'px',
+                    left: ((window.innerWidth - width) * 0.5) + 'px'
+                }
+            };
+
+            image.src = this.image;
+
+        }
 
     }
 
