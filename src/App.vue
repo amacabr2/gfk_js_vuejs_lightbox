e514dec368e6614b9cfbd35939db786247fb359b
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-04-23 15:55:04 +0200

    Add framework semantic ui for the main design

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,34 @@
 
     <div id="app">
 
-        <div v-if="title === 'futurama'">
-            <a :href="'http://lorempicsum.com/futurama/800/600/' + n" v-lightbox v-for="n in 9">
-                <img :src="'http://lorempicsum.com/futurama/150/150/' + n" alt="">
-            </a>
-            <lightbox></lightbox>
+        <div class="ui tabular menu">
+            <div class="item" @click="title='rio'" :class="{active: title == 'rio'}">RIO</div>
+            <div class="item" @click="title='futurama'" :class="{active: title == 'futurama'}">FUTURAMA</div>
         </div>
 
-        <section>
-            <div v-if="title === 'rio'">
-                <a :href="'http://lorempicsum.com/rio/800/600/' + n" v-lightbox v-for="n in 9">
-                    <img :src="'http://lorempicsum.com/rio/150/150/' + n" alt="">
+        <br>
+
+        <div class="row">
+
+            <div class="ui grid" v-if="title === 'futurama'">
+                <a :href="'http://lorempicsum.com/futurama/800/600/' + n" v-lightbox v-for="n in 9">
+                    <div class="four wide column">
+                        <img :src="'http://lorempicsum.com/futurama/150/150/' + n" alt="">
+                    </div>
                 </a>
                 <lightbox></lightbox>
             </div>
-        </section>
 
-        <button @click="title= 'rio'">RIO</button>
-        <button @click="title= 'futurama'">FUTURAMA</button>
+            <section>
+                <div class="ui grid" v-if="title === 'rio'">
+                    <a :href="'http://lorempicsum.com/rio/800/600/' + n" v-lightbox v-for="n in 9">
+                        <img :src="'http://lorempicsum.com/rio/150/150/' + n" alt="">
+                    </a>
+                    <lightbox></lightbox>
+                </div>
+            </section>
+
+        </div>
 
     </div>
 
@@ -48,6 +58,7 @@
 </script>
 
 <style>
+
     #app {
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
@@ -56,4 +67,9 @@
         color: #2c3e50;
         margin-top: 60px;
     }
+
+    .item {
+        cursor: pointer;
+    }
+
 </style>
